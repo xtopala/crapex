73548e85da73b9c002e1fbfaa45f4179621b2ea4
xtopala <xtopala@gmail.com>
2021-10-23 11:34:09 +0200

    making the Camera move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform viewPoint;
+    public float mouseSensitivity = 1f;
+    private float verticalRotStore;
+    private Vector2 mouseInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xPos = Input.GetAxisRaw("Mouse X");
+        float yPos = Input.GetAxisRaw("Mouse Y");
+        mouseInput = new Vector2(xPos, yPos) * mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+    }
+}
