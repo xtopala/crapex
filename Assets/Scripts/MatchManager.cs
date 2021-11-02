27bd8654385c8ecef2b327c24e8b4ca76d64d205
xtopala <xtopala@gmail.com>
2021-11-02 18:23:39 +0100

    setting up Match Manager Systems

--- Assets/Scripts/MatchManager.cs
+++ Assets/Scripts/MatchManager.cs
@@ -1,12 +1,22 @@
-using Photon.Pun;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using Photon.Realtime;
 using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class MatchManager : MonoBehaviourPunCallbacks, IOnEventCallback
 {
     public static MatchManager instance;
+
+    public List<PlayerInfo> allPlayers = new List<PlayerInfo>();
+    private int index;
+
+    public enum EventCodes : byte
+    {
+        NewPlayer,
+        ListPlayer,
+        ChangeStat,
+    }
 
     private void Awake()
     {
@@ -34,5 +44,20 @@ public class MatchManager : MonoBehaviourPunCallbacks, IOnEventCallback
     public override void OnDisable()
     {
         PhotonNetwork.RemoveCallbackTarget(this);
+    }
+}
+
+[System.Serializable]
+public class PlayerInfo
+{
+    public string name;
+    public int actor, kills, deaths;
+
+    public PlayerInfo(string _name, int _actor, int _kills, int _deaths)
+    {
+        name = _name;
+        actor = _actor;
+        kills = _kills;
+        deaths = _deaths;
     }
 }
