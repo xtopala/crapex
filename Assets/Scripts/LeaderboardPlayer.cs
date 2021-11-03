3da77f57e479401ee1195086646dbdb61bf70b70
xtopala <xtopala@gmail.com>
2021-11-03 20:04:17 +0100

    Laying out the Leaderboard

---
+++ Assets/Scripts/LeaderboardPlayer.cs
@@ -0,0 +1,14 @@
+using TMPro;
+using UnityEngine;
+
+public class LeaderboardPlayer : MonoBehaviour
+{
+    public TMP_Text playerNameText, killsText, deathsText;
+
+    public void SetDetails(string name, int kills, int deaths)
+    {
+        playerNameText.text = name;
+        killsText.text = kills.ToString();
+        deathsText.text = deaths.ToString();
+    }
+}
